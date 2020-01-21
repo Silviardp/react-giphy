e2fffacc72e54575d692e534fdd4ffcbe9940433
Silvia Rocha <silviadprocha@gmail.com>
2020-01-21 19:05:52 +0700

    add state

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 import "../assets/stylesheets/application.scss";
 
-const Hello = props => {
-  return <div>Hello, {props.name}</div>;
-};
+// const Hello = props => {
+//   return <div>Hello, {props.name}</div>;
+// };
+
+class Hello extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      clicked: true,
+      counter: 0
+    };
+  }
+
+  handleClick = () => {
+    //change the state
+    this.setState({
+      clicked: !this.state.clicked,
+      counter: this.state.counter + 1
+    });
+  };
+
+  render() {
+    // build and return html
+    return (
+      <div
+        className={this.state.clicked ? "clicked" : null}
+        onClick={this.handleClick}
+      >
+        Hello {this.props.name} {this.state.counter}
+      </div>
+    );
+  }
+}
 
 const root = document.getElementById("root");
 if (root) {
