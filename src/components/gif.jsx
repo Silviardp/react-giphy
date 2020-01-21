d3c8022ad6643923a9df0b5b20b214a5ab185429
Silvia Rocha <silviadprocha@gmail.com>
2020-01-21 20:04:49 +0700

    added components app gif and searchbar

---
+++ src/components/gif.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Gif extends Component {
+  render() {
+    return (
+      const src = "https://media3.giphy.com/media/VbhTiwsWCYSnw1ZNQu/giphy.gif"
+      <img
+        src={src}
+        alt=""
+        className="gif"
+      />
+    );
+  }
+}
+
+export default Gif;
--- src/index.jsx
+++ src/index.jsx
@@ -2,43 +2,9 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 import "../assets/stylesheets/application.scss";
-
-// const Hello = props => {
-//   return <div>Hello, {props.name}</div>;
-// };
-
-class Hello extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      clicked: true,
-      counter: 0
-    };
-  }
-
-  handleClick = () => {
-    //change the state
-    this.setState({
-      clicked: !this.state.clicked,
-      counter: this.state.counter + 1
-    });
-  };
-
-  render() {
-    // build and return html
-    return (
-      <div
-        className={this.state.clicked ? "clicked" : null}
-        onClick={this.handleClick}
-      >
-        Hello {this.props.name} {this.state.counter}
-      </div>
-    );
-  }
-}
+import App from "./components/app.jsx";
 
 const root = document.getElementById("root");
 if (root) {
-  ReactDOM.render(<Hello name="Silvia" />, root);
+  ReactDOM.render(<App />, root);
 }
