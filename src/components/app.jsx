5bb4872c17d5402b6fb2bd2d599c55beb018d559
Silvia Rocha <silviadprocha@gmail.com>
2020-01-22 18:34:45 +0700

    finished react giphy exercise

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,21 +1,48 @@
 import React, { Component } from "react";
+import giphy from "giphy-api";
+
 import SearchBar from "./search_bar";
 import Gif from "./gif";
 import GifList from "./gif_list";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "VbhTiwsWCYSnw1ZNQu"
+    };
+
+    this.search("disney");
+  }
+
+  search = query => {
+    giphy("IAWKwh5zhMRmKXCI9idiz6I82PDQvDVP").search(
+      {
+        q: query,
+        rating: "g",
+        limit: 10
+      },
+      (error, result) => {
+        this.setState({
+          gifs: result.data
+        });
+      }
+    );
+  };
+
   render() {
-    const gifs = [{ id: "VbhTiwsWCYSnw1ZNQu" }, { id: "fvTsVlUzFgVNdpoGpQ" }];
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
           <div className="selected-gif">
-            <Gif id="VbhTiwsWCYSnw1ZNQu" />
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={gifs} />
+          <GifList gifs={this.state.gifs} />
         </div>
       </div>
     );
